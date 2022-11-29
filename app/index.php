d501c0cf081cdace675eba48e6c1136c0c08969b
merpich <merpich.dev@gmail.com>
2022-11-29 22:49:57 +0300

    Добавлены заголовки CORS

--- app/index.php
+++ app/index.php
@@ -1,5 +1,10 @@
 <?php
 
+header("Access-Control-Allow-Origin: http://localhost:3000/");
+header("Access-Control-Allow-Methods: *");
+header("Access-Control-Allow-Headers: *");
+header("Content-Type: application/json; charset=UTF-8");
+
 require_once "../config/database.php";
 
 require_once "../vendor/autoload.php";
